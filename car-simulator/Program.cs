4eba8bee94d43aa9a593e6019ccec16e7bca462e
clinnyp <cphilathong1@gmail.com>
2026-01-08 00:17:00 +1300

    fix toggled charging for whole minute bug

--- car-simulator/Program.cs
+++ car-simulator/Program.cs
@@ -14,6 +14,7 @@ using var deviceClient = DeviceClient.CreateFromConnectionString(connectionStrin
 await deviceClient.OpenAsync();
 Console.WriteLine("Connected!");
 
+// Initialize reported properties
 var reportedProperties = new TwinCollection();
 var random = new Random();
 var _batteryLevel = random.Next(20, 51);
@@ -24,47 +25,66 @@ reportedProperties["isCharging"] = _isCharging;
 await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
 await deviceClient.SetMethodHandlerAsync("ToggleCharging", ToggleCharging, null);
 
+
+// Registering callback for scheduled charging
+DateTime? _scheduledChargeTime = null;
+await deviceClient.SetDesiredPropertyUpdateCallbackAsync(ScheduledChargeCallback, null);
+
 while (true) {
   await Task.Delay(3000);
 
-  var twin = await deviceClient.GetTwinAsync();
-  var desiredProperties = twin.Properties.Desired;
-  
-  if (desiredProperties.Contains("scheduleCharge"))
+  // Schedule Charing Simulation
+  if (_scheduledChargeTime.HasValue)
   {
-    var timeString = desiredProperties["scheduleCharge"]?.ToString();
-
-    if (DateTime.TryParse(timeString, out DateTime scheduledCharge))
+    var now = DateTime.UtcNow;
+    if (now.Date == _scheduledChargeTime.Value.Date &&
+        now.Hour == _scheduledChargeTime.Value.Hour &&
+        now.Minute == _scheduledChargeTime.Value.Minute &&
+    !_isCharging) 
     {
-      scheduledCharge = scheduledCharge.ToUniversalTime();
-      var now = DateTime.UtcNow;
-
-      if (scheduledCharge.Date == now.Date && 
-          scheduledCharge.Hour == now.Hour && 
-          scheduledCharge.Minute == now.Minute && 
-          !_isCharging)
-      {
-        _isCharging = true;
-        reportedProperties["isCharging"] = _isCharging;
-        await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
-        Console.WriteLine("Starting scheduled charging");
-      }
+      _isCharging = true;
+      reportedProperties["isCharging"] = _isCharging;
+      await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+      _scheduledChargeTime = null;
+      Console.WriteLine("Starting scheduled charging");
     }
-  } 
- 
+  }
+  
+  // Charging simulation
   if (_isCharging && _batteryLevel < 100) {
     _batteryLevel++;
     reportedProperties["batteryLevel"] = _batteryLevel;
     await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
     Console.WriteLine($"Battery level: {_batteryLevel}%");
   } 
-  
+
+  // Discharging simulation
   if (!_isCharging && _batteryLevel > 0) {
     _batteryLevel--;
     reportedProperties["batteryLevel"] = _batteryLevel;
     await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
     Console.WriteLine($"Battery level: {_batteryLevel}%");
   }  
+}
+
+Task ScheduledChargeCallback(TwinCollection desiredProperties, object userContext)
+{
+  if (desiredProperties.Contains("scheduleCharge"))
+  {
+    var timeString = desiredProperties["scheduleCharge"]?.ToString();
+    if (DateTime.TryParse(timeString, out DateTime scheduledCharge))
+    {
+      scheduledCharge = scheduledCharge.ToUniversalTime();
+      _scheduledChargeTime = scheduledCharge;
+      Console.WriteLine($"Scheduled charge time: {_scheduledChargeTime.Value.ToString("HH:mm")}");
+    }
+  }
+  else 
+  {
+    _scheduledChargeTime = null;
+    Console.WriteLine("Scheduled charge time cleared");
+  }
+  return Task.CompletedTask;
 }
 
 async Task<MethodResponse> ToggleCharging(MethodRequest methodRequest, object userContext) {
